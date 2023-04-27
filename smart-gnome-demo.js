b48c34fa9ea7b95c0054a461ae58234f58b9d7f8
Josh Bucci <85126980+Ataraxiis@users.noreply.github.com>
2023-04-26 20:21:11 -0600

    Updating to New SVG & JS

--- smart-gnome-demo.js
+++ smart-gnome-demo.js
@@ -1,11 +1,12 @@
 document.getElementById('svg-object').addEventListener('load', function() {
     const svgDocument = this.contentDocument;
     const lightBedroom = svgDocument.getElementById('light_bedroom');
-
-    if (lightBedroom) {
-      lightBedroom.addEventListener('click', () => {
+    const buttonBedroom = svgDocument.getElementById('button_bedroom');
+  
+    if (lightBedroom && buttonBedroom) {
+      buttonBedroom.addEventListener('click', () => {
         const currentOpacity = lightBedroom.style.opacity;
-
+  
         if (currentOpacity === '0') {
           lightBedroom.style.opacity = '1';
         } else {
@@ -14,3 +15,4 @@ document.getElementById('svg-object').addEventListener('load', function() {
       });
     }
   });
+  
