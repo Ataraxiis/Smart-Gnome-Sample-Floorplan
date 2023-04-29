3ff940caafdcf7896e41df3f98319f1382075f20
Josh Bucci <85126980+Ataraxiis@users.noreply.github.com>
2023-04-29 14:50:03 -0600

    Updated Overhaul View

--- smart-gnome-demo.js
+++ smart-gnome-demo.js
@@ -6,22 +6,10 @@ document.getElementById('svg-object').addEventListener('load', function() {
     const buttonDoorLock = svgDocument.getElementById('button_door_lock');
     const bgDoorLocked = svgDocument.getElementById('bg_door_locked');
     const bgDoorUnlocked = svgDocument.getElementById('bg_door_unlocked');
-    bgDoorUnlocked.style.display = 'none';
-    bgDoorUnlocked.style.opacity = 1;
-
-    buttonDoorLock.addEventListener('click', () => {
-      if (bgDoorLocked.style.display === 'none') {
-        bgDoorLocked.style.display = 'inline';
-        bgDoorUnlocked.style.display = 'none'
-      } else {
-        bgDoorLocked.style.display = 'none';
-        bgDoorUnlocked.style.display = 'inline';
-      }
-    });
-    
-
-
-    /*Rooms, Lights, & Camera*/
+    const itemLock = svgDocument.getElementById('item_lock');
+    const itemLocked = svgDocument.getElementById('item_locked');
+    const itemUnlocked = svgDocument.getElementById('item_unlocked');
+    /*Rooms, Lights, & Camera on Home Layout*/
     const lightBedroom = svgDocument.getElementById('light_bedroom');
     const buttonBedroom = svgDocument.getElementById('button_bedroom');
     const lightKitchen = svgDocument.getElementById('light_kitchen');
@@ -38,8 +26,79 @@ document.getElementById('svg-object').addEventListener('load', function() {
     const hexMenu = svgDocument.getElementById('layer6');
     const hexLightButton = svgDocument.getElementById('hex_light_button');
     const lightLivingRoom = svgDocument.getElementById('light_livingroom')
+    const itemizedView = svgDocument.getElementById('itemized_view');
+    const itemizedViewButton = svgDocument.getElementById('button_itemized_view')
+    const floorPlanViewButton = svgDocument.getElementById('button_floor_plan_view')
+    const item_closet = svgDocument.getElementById('item_closet');
+    const item_kitchen = svgDocument.getElementById('item_kitchen');
+    const item_garden = svgDocument.getElementById('item_garden');
+    const item_spareroom = svgDocument.getElementById('item_spareroom');
+    const item_bedroom = svgDocument.getElementById('item_bedroom');
+    const item_livingroom = svgDocument.getElementById('item_livingroom');
+    const itemClosetOff = svgDocument.getElementById('item_closet_off');
+    const itemClosetOn = svgDocument.getElementById('item_closet_on');
+    const itemKitchenOff = svgDocument.getElementById('item_kitchen_off');
+    const itemKitchenOn = svgDocument.getElementById('item_kitchen_on');
+    const itemGardenOff = svgDocument.getElementById('item_garden_off');
+    const itemGardenOn = svgDocument.getElementById('item_garden_on');
+    const itemSpareroomOff = svgDocument.getElementById('item_spareroom_off');
+    const itemSpareroomOn = svgDocument.getElementById('item_spareroom_on');
+    const itemBedRoomOff = svgDocument.getElementById('item_bedroom_off');
+    const itemBedRoomOn = svgDocument.getElementById('item_bedroom_on');
+    const itemLivingRoomOff = svgDocument.getElementById('item_living_room_off');
+    const itemLivingRoomOn = svgDocument.getElementById('item_living_room_on');
+
+    /*set starting states*/
     let hexLightButtonOpacity = 0.28;
+    bgDoorUnlocked.style.display = 'none';
+    itemUnlocked.style.display = 'none';
+    bgDoorUnlocked.style.opacity = 1;
+    itemizedView.style.display = 'none';
+    floorPlanViewButton.style.opacity = 1;
+    itemizedViewButton.style.opacity = .24;
+    itemClosetOff.style.display = 'inline';
+    itemClosetOn.style.display = 'none';
+    itemKitchenOff.style.display = 'inline';
+    itemKitchenOn.style.display = 'none';
+    itemGardenOff.style.display = 'inline';
+    itemGardenOn.style.display = 'none';
+    itemSpareroomOff.style.display = 'inline';
+    itemSpareroomOn.style.display = 'none';
+    itemLivingRoomOff.style.display = 'inline';
+    itemLivingRoomOn.style.display = 'none';
+    itemBedRoomOff.style.display = 'inline';
+    itemBedRoomOn.style.display = 'none';
+
+    /*swap views*/
+    itemizedViewButton.addEventListener('click', () => {
+      itemizedView.style.display = 'inline';
+      itemizedViewButton.style.opacity = 1;
+      floorPlanViewButton.style.opacity = .24;
+    });
+
+    floorPlanViewButton.addEventListener('click', () => {
+      itemizedView.style.display = 'none';
+      floorPlanViewButton.style.opacity = 1;
+      itemizedViewButton.style.opacity = .24;
+    });
+
+    /*interactions*/
     
+    buttonDoorLock.addEventListener('click', () => {
+      if (bgDoorLocked.style.display === 'none') {
+        bgDoorLocked.style.display = 'inline';
+        itemLocked.style.display = 'inline';
+        bgDoorUnlocked.style.display = 'none';
+        itemUnlocked.style.display = 'none';
+      } else {
+        bgDoorLocked.style.display = 'none';
+        itemLocked.style.display = 'none';
+        bgDoorUnlocked.style.display = 'inline';
+        itemUnlocked.style.display = 'inline';
+      }
+    });
+
+
     hexButton.addEventListener('click', () => {
       if (hexMenu.style.display === 'none') {
         hexMenu.style.display = 'inline';
@@ -66,8 +125,12 @@ document.getElementById('svg-object').addEventListener('load', function() {
   
         if (currentOpacity === '0') {
           lightBedroom.style.opacity = '1';
+          itemBedRoomOn.style.display = 'inline';
+          itemBedRoomOff.style.display = 'none';
         } else {
           lightBedroom.style.opacity = '0';
+          itemBedRoomOn.style.display = 'none';
+          itemBedRoomOff.style.display = 'inline';
         }
       });
     }
@@ -79,8 +142,12 @@ document.getElementById('svg-object').addEventListener('load', function() {
   
         if (currentOpacity === '0') {
           lightKitchen.style.opacity = '1';
+          itemKitchenOn.style.display = 'none';
+          itemKitchenOff.style.display = 'inline';
         } else {
           lightKitchen.style.opacity = '0';
+          itemKitchenOn.style.display = 'inline';
+          itemKitchenOff.style.display = 'none';
         }
       });
     }
@@ -91,8 +158,12 @@ document.getElementById('svg-object').addEventListener('load', function() {
   
         if (currentOpacity === '0') {
           lightGarden.style.opacity = '1';
+          itemGardenOn.style.display = 'none';
+          itemGardenOff.sytle.display = 'inline';
         } else {
           lightGarden.style.opacity = '0';
+          itemGardenOn.style.display = 'inline';
+          itemGardenOff.sytle.display = 'none';
         }
       });
     }
@@ -103,8 +174,12 @@ document.getElementById('svg-object').addEventListener('load', function() {
   
         if (currentOpacity === '0') {
           lightCloset.style.opacity = '1';
+          itemClosetOn.style.display = 'none';
+          itemClosetOff.style.display = 'inline';
         } else {
           lightCloset.style.opacity = '0';
+          itemClosetOn.style.display = 'inline';
+          itemClosetOff.style.display = 'none';
         }
       });
     }
@@ -115,8 +190,12 @@ document.getElementById('svg-object').addEventListener('load', function() {
   
         if (currentOpacity === '0') {
           lightSpareRoom.style.opacity = '1';
+          itemSpareroomOn.style.display = 'none';
+          itemBedRoomOff.style.display = 'inline';
         } else {
           lightSpareRoom.style.opacity = '0';
+          itemSpareroomOn.style.display = 'inline';
+          itemSpareroomOff.style.display = 'none';
         }
       });
     }
@@ -139,17 +218,104 @@ document.getElementById('svg-object').addEventListener('load', function() {
       
             if (currentOpacity === '0') {
               lightLivingRoom.style.opacity = '1';
+              itemLivingRoomOn.style.display = 'none';
+              itemLivingRoomOff.style.display = 'inline';
             } else {
               lightLivingRoom.style.opacity = '0';
+              itemLivingRoomOn.style.display = 'inline';
+              itemLivingRoomOff.style.display = 'none';
             }
           });
         }
 
 
+    item_bedroom.addEventListener('click', () => {
+        const currentOpacity = lightBedroom.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightBedroom.style.opacity = '1';
+          itemBedRoomOn.style.display = 'inline';
+          itemBedRoomOff.style.display = 'none';
+        } else {
+          lightBedroom.style.opacity = '0';
+          itemBedRoomOn.style.display = 'none';
+          itemBedRoomOff.style.display = 'inline';
+        }
+      });
+    
+    item_kitchen.addEventListener('click', () => {
+        const currentOpacity = lightKitchen.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightKitchen.style.opacity = '1';
+          itemKitchenOn.style.display = 'none';
+          itemKitchenOff.style.display = 'inline';
+        } else {
+          lightKitchen.style.opacity = '0';
+          itemKitchenOn.style.display = 'inline';
+          itemKitchenOff.style.display = 'none';
+        }
+      });
 
+
+    item_garden.addEventListener('click', () => {
+        const currentOpacity = lightGarden.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightGarden.style.opacity = '1';
+          itemGardenOn.style.display = 'none';
+          itemGardenOff.sytle.display = 'inline';
+        } else {
+          lightGarden.style.opacity = '0';
+          itemGardenOn.style.display = 'inline';
+          itemGardenOff.sytle.display = 'none';
+        }
+      });
+
+
+    item_closet.addEventListener('click', () => {
+        const currentOpacity = lightCloset.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightCloset.style.opacity = '1';
+          itemClosetOn.style.display = 'none';
+          itemClosetOff.style.display = 'inline';
+        } else {
+          lightCloset.style.opacity = '0';
+          itemClosetOn.style.display = 'inline';
+          itemClosetOff.style.display = 'none';
+        }
+      });
 
     
+    item_spareroom.addEventListener('click', () => {
+        const currentOpacity = lightSpareRoom.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightSpareRoom.style.opacity = '1';
+          itemSpareroomOn.style.display = 'none';
+          itemBedRoomOff.style.display = 'inline';
+        } else {
+          lightSpareRoom.style.opacity = '0';
+          itemSpareroomOn.style.display = 'inline';
+          itemSpareroomOff.style.display = 'none';
+        }
+      });
 
+
+    item_livingroom.addEventListener('click', () => {
+        const currentOpacity = lightLivingRoom.style.opacity;
+  
+        if (currentOpacity === '0') {
+          lightLivingRoom.style.opacity = '1';
+          itemLivingRoomOn.style.display = 'none';
+          itemLivingRoomOff.style.display = 'inline';
+        } else {
+          lightLivingRoom.style.opacity = '0';
+          itemLivingRoomOn.style.display = 'inline';
+          itemLivingRoomOff.style.display = 'none';
+        }
+      });
 
   });
   
